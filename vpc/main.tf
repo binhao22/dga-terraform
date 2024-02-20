87499930ae25e0ceb996cf0f517b070c1681ba88
HoneyBIN <binhao@naver.com>
2024-02-20 15:31:09 +0900

    test create vpc

---
+++ vpc/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+}
+
+# Create a VPC
+resource "aws_vpc" "dev-terraform" {
+  cidr_block = "10.0.0.0/16"
+}
+
+
