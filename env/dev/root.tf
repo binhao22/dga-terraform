8bafc8425526c181469c527ddf9454d165fafd76
HoneyBIN <binhao@naver.com>
2024-02-22 15:17:12 +0900

    test

--- env/dev/root.tf
+++ env/dev/root.tf
@@ -12,7 +12,7 @@ module "dga-vpc" {
 #
 
 
-# test_create_ec2###
+# test_create_ec2####
 resource "aws_instance" "test_tf" {
   subnet_id  = module.dga-vpc.dga-pub-1_id
   ami           = "ami-0c76973fbe0ee100c"
