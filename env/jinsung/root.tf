43a5eabda74a7e2105472a0c508a372e41566e7b
winnertree <winnertree@naver.com>
2024-02-22 16:52:53 +0900

    change data resoure

--- env/jinsung/root.tf
+++ env/jinsung/root.tf
@@ -1,6 +1,15 @@
+data "output.tf" "dev" {
+  organization = "DGA-PROJECT"
+  workspace = "dga-terraform"
+}
+
 ## test_create_ec2
 resource "aws_instance" "test_tf" {
-  subnet_id  = module.dga-vpc.dga-pub-1_id
+  keepers = {
+    # Generate a new ID any time the value of 'bar' in workspace 'my-org/my-workspace' changes.
+    subnet_id = data.tfe_outputs.dev.values.dga-pub-1_id
+  }
+#  subnet_id  = module.dga-vpc.dga-pub-1_id
   ami           = "ami-0c76973fbe0ee100c"
   instance_type = "t2.nano"
   associate_public_ip_address = true
