20ab4f1dd1b8bd6b94c98e436b294d9d60eb30cb
HoneyBIN <binhao@naver.com>
2024-02-22 17:21:19 +0900

    go

--- env/daewon/root.tf
+++ env/daewon/root.tf
@@ -1,11 +1,18 @@
-# test_create_ec2 ###
+data "tfe_outputs" "dev" {
+  organization = "DGA-PROJECT"
+  workspace = "dga-terraform"
+}
+
+## test_create_ec2
 resource "aws_instance" "test_tf" {
-  subnet_id  = module.dga-vpc.dga-pub-1_id
+
+  subnet_id = data.tfe_outputs.dev.values.dga-pub-1_id
   ami           = "ami-0c76973fbe0ee100c"
   instance_type = "t2.nano"
   associate_public_ip_address = true
 
   tags = {
-    Name = "test_tf"
+    Name = "test_daewon"
   }
 }
+### 
