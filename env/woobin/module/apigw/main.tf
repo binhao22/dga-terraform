200196009e256b8c09e9caef11898091f1c848d7
HoneyBIN <binhao@naver.com>
2024-02-26 17:29:41 +0900

    apigw

--- env/woobin/module/apigw/main.tf
+++ env/woobin/module/apigw/main.tf
@@ -25,7 +25,7 @@ resource "aws_api_gateway_resource" "boards" {
   parent_id   = aws_api_gateway_rest_api.dga-apigw.root_resource_id
   path_part   = "boards"
 }
-module "community_cors" {
+module "community_cors2" {
   source  = "squidfunk/api-gateway-enable-cors/aws"
   version = "0.3.3"
   api_id          = aws_api_gateway_rest_api.dga-apigw.id
