bb18aed4b175d3a95bccf650fe8abcec3fcd2965
HoneyBIN <binhao@naver.com>
2024-02-26 16:08:37 +0900

    apigw

--- env/woobin/module/apigw/main.tf
+++ env/woobin/module/apigw/main.tf
@@ -11,7 +11,7 @@ module "community_cors" {
   version = "0.3.3"
 
   api_id          = aws_api_gateway_rest_api.dga-apigw.id
-  api_resource_id = [aws_api_gateway_rest_api.dga-apigw.root_resource_id, aws_api_gateway_resource.dga-apigw-boards.id]
+  api_resource_id = [aws_api_gateway_rest_api.dga-apigw.root_resource_id]
 }
 
 resource "aws_api_gateway_resource" "dga-apigw-boards" {
@@ -19,3 +19,48 @@ resource "aws_api_gateway_resource" "dga-apigw-boards" {
   path_part   = "boards"
   rest_api_id = aws_api_gateway_rest_api.dga-apigw.id
 }
+
+/*
+resource "aws_api_gateway_method" "example" {
+  authorization = "NONE"
+  http_method   = "GET"
+  resource_id   = aws_api_gateway_resource.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+}
+
+resource "aws_api_gateway_integration" "example" {
+  http_method = aws_api_gateway_method.example.http_method
+  resource_id = aws_api_gateway_resource.example.id
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  type        = "MOCK"
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.example.id,
+      aws_api_gateway_method.example.id,
+      aws_api_gateway_integration.example.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "example" {
+  deployment_id = aws_api_gateway_deployment.example.id
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  stage_name    = "example"
+}
+*/
