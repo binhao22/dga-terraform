858eaeea9578282477a2760deaa10b1fb8b90022
HoneyBIN <binhao@naver.com>
2024-02-26 16:01:10 +0900

    apigw

--- env/woobin/module/apigw/main.tf
+++ env/woobin/module/apigw/main.tf
@@ -1,14 +1,26 @@
 resource "aws_api_gateway_rest_api" "dga-apigw" {
   name = "dga-apigw"
+
+  tags = {
+    name = "dga-apigw"
+  }
+}
+
+module "community_cors" {
+  source  = "squidfunk/api-gateway-enable-cors/aws"
+  version = "0.3.3"
+
+  api_id          = aws_api_gateway_rest_api.dga-apigw.id
+  api_resource_id = [aws_api_gateway_rest_api.dga-apigw.root_resource_id, aws_api_gateway_resource.dga-apigw-boards.id]
+}
+
+resource "aws_api_gateway_resource" "dga-apigw-boards" {
+  parent_id   = aws_api_gateway_rest_api.dga-apigw.root_resource_id
+  path_part   = "boards"
+  rest_api_id = aws_api_gateway_rest_api.dga-apigw.id
 }
 
 /*
-resource "aws_api_gateway_resource" "example" {
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "example"
-  rest_api_id = aws_api_gateway_rest_api.example.id
-}
-
 resource "aws_api_gateway_method" "example" {
   authorization = "NONE"
   http_method   = "GET"
