c92276a9868dd8e5589c2b80199a3788a84d3afc
HoneyBIN <binhao@naver.com>
2024-02-26 17:30:22 +0900

    apigw

--- env/woobin/module/apigw/main.tf
+++ env/woobin/module/apigw/main.tf
@@ -43,7 +43,7 @@ resource "aws_api_gateway_integration" "boards" {
   resource_id = aws_api_gateway_resource.boards.id
   rest_api_id = aws_api_gateway_rest_api.dga-apigw.id
   type                    = "HTTP"
-  uri                     = var.dga-nlb-id + "/boards"
+  uri                     = var.dga-nlb-id/boards
   integration_http_method = "GET"
   connection_type = "VPC_LINK"
   connection_id   = aws_api_gateway_vpc_link.dga-vpclink.id
