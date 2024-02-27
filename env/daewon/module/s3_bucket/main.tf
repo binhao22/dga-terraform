7656b58acf81cb479e1d0e7b30a84efceb9a508f
brokennose <ths8976@naver.com>
2024-02-27 19:34:22 +0900

    son update s3 main.tf

--- env/daewon/module/s3_bucket/main.tf
+++ env/daewon/module/s3_bucket/main.tf
@@ -62,21 +62,21 @@ resource "aws_s3_bucket_acl" "my_bucket_acl" {
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.id
+# resource "aws_s3_bucket_policy" "my_bucket_policy" {
+#   bucket = aws_s3_bucket.my_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = ["s3:GetObject"]
+#         Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_s3_bucket_cors_rule" "cors_rule" {
   bucket = aws_s3_bucket.my_bucket.id
