466442305c8cfdbc8092e1ad19f2cba808b0f960
HoneyBIN <binhao@naver.com>
2024-02-27 14:30:55 +0900

    apigw

--- env/woobin/module/apigw/main.tf
+++ env/woobin/module/apigw/main.tf
@@ -44,7 +44,7 @@ resource "aws_api_gateway_integration" "proxy" {
   http_method = aws_api_gateway_method.any.http_method
   resource_id = aws_api_gateway_resource.proxy.id
   rest_api_id = aws_api_gateway_rest_api.dga-apigw.id
-  type                    = "HTTP"
+  type                    = "HTTP_PROXY"
   uri                     = format("%s%s%s", "http://", var.dga-nlb-dns, "/{proxy}")
   integration_http_method = "ANY"
   connection_type = "VPC_LINK"
