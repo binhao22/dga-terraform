c90929efc4dc8b646fa7f3ab251e493be6576d72
HoneyBIN <binhao@naver.com>
2024-02-27 14:48:57 +0900

    apigw

--- env/woobin/module/apigw/main.tf
+++ env/woobin/module/apigw/main.tf
@@ -39,6 +39,10 @@ resource "aws_api_gateway_method" "any" {
   http_method   = "ANY"
   resource_id   = aws_api_gateway_resource.proxy.id
   rest_api_id   = aws_api_gateway_rest_api.dga-apigw.id
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
 }
 resource "aws_api_gateway_integration" "proxy" {
   http_method = aws_api_gateway_method.any.http_method
@@ -49,26 +53,21 @@ resource "aws_api_gateway_integration" "proxy" {
   integration_http_method = "ANY"
   connection_type = "VPC_LINK"
   connection_id   = aws_api_gateway_vpc_link.dga-vpclink.id
+  cache_key_parameters = ["method.request.path.proxy"]
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
 }
 
-
-
-/*
-resource "aws_api_gateway_deployment" "example" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
+resource "aws_api_gateway_deployment" "dep" {
+  rest_api_id = aws_api_gateway_rest_api.dga-apigw.id
 
   triggers = {
-    # NOTE: The configuration below will satisfy ordering considerations,
-    #       but not pick up all future REST API changes. More advanced patterns
-    #       are possible, such as using the filesha1() function against the
-    #       Terraform configuration file(s) or removing the .id references to
-    #       calculate a hash against whole resources. Be aware that using whole
-    #       resources will show a difference after the initial implementation.
-    #       It will stabilize to only change when resources change afterwards.
     redeployment = sha1(jsonencode([
-      aws_api_gateway_resource.example.id,
-      aws_api_gateway_method.example.id,
-      aws_api_gateway_integration.example.id,
+      aws_api_gateway_resource.proxy.id,
+      aws_api_gateway_method.any.id,
+      aws_api_gateway_integration.proxy.id,
     ]))
   }
 
@@ -77,9 +76,8 @@ resource "aws_api_gateway_deployment" "example" {
   }
 }
 
-resource "aws_api_gateway_stage" "example" {
-  deployment_id = aws_api_gateway_deployment.example.id
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  stage_name    = "example"
+resource "aws_api_gateway_stage" "api" {
+  deployment_id = aws_api_gateway_deployment.dep.id
+  rest_api_id   = aws_api_gateway_rest_api.dga-apigw.id
+  stage_name    = "api"
 }
-*/
