c851e82c80e5e290ccb2418d38f92bfc56819d0b
brokennose <ths8976@naver.com>
2024-02-27 18:56:34 +0900

    son root.tf module chage

--- env/daewon/root.tf
+++ env/daewon/root.tf
@@ -3,9 +3,22 @@ data "tfe_outputs" "woobin" {
   workspace = "woobin"
 }
 
-
 module "s3_bucket" {
-  source = "./module/s3_bucket"
-  bucket_name = "test.brokennose.shop"
-  bucket_acl  = "public-read-write"
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "test.brokennosetest.shop"
+  acl    = "public-read-write"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
 }
+
+# module "s3_bucket" {
+#   source = "./module/s3_bucket"
+#   bucket_name = "test.brokennose.shop"
+#   bucket_acl  = "public-read-write"
+# }
