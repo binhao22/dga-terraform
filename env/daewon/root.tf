3afcab964b820fec9659d0780fa12f4ddbb6db6b
brokennose <ths8976@naver.com>
2024-02-27 10:01:28 +0900

    daewon module folder add

--- env/daewon/root.tf
+++ env/daewon/root.tf
@@ -3,15 +3,3 @@ data "tfe_outputs" "woobin" {
   workspace = "woobin"
 }
 
-## test_create_ec2
-resource "aws_instance" "test_tf" {
-
-  subnet_id = data.tfe_outputs.woobin.values.dga-pub-1-id
-  ami           = "ami-0c76973fbe0ee100c"
-  instance_type = "t2.nano"
-  associate_public_ip_address = true
-
-  tags = {
-    Name = "test_tf"
-  }
-}
