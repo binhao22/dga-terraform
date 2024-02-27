c13cf4869b470ef72e9a8c7289997685ddc2a145
brokennose <ths8976@naver.com>
2024-02-27 19:25:44 +0900

    son update s3 main.tf

--- env/daewon/module/s3_bucket/main.tf
+++ env/daewon/module/s3_bucket/main.tf
@@ -36,12 +36,30 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "bucket-public-access-block" {
+resource "aws_s3_bucket_public_access_block" "aws_s3_bucket-public-access-block-test" {
   bucket                  = aws_s3_bucket.my_bucket.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_ownership_controls_test" {
+  bucket = aws_s3_bucket.my_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+# aws s3 bucket acl
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.aws_s3_bucket_ownership_controls_test,
+    aws_s3_bucket_public_access_block.aws_s3_bucket-public-access-block-test,
+  ]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "my_bucket_policy" {
