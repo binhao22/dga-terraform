66da4397373fc2e5940a4e17c8d237f5c2b7aa84
brokennose <ths8976@naver.com>
2024-02-27 19:10:51 +0900

    son update s3 > main.tf add public-access-block

--- env/daewon/module/s3_bucket/main.tf
+++ env/daewon/module/s3_bucket/main.tf
@@ -37,6 +37,13 @@ resource "aws_s3_bucket" "my_bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket-public-access-block" {
+  bucket                  = aws_s3_bucket.my_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 resource "aws_s3_bucket_policy" "my_bucket_policy" {
   bucket = aws_s3_bucket.my_bucket.id
