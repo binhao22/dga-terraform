b71d64d747e436d177cf51611b6be30c18be62fc
HoneyBIN <binhao@naver.com>
2024-02-27 14:10:28 +0900

    apigw

--- env/woobin/module/apigw/main.tf
+++ env/woobin/module/apigw/main.tf
@@ -21,7 +21,7 @@ module "community_cors" {
   source  = "squidfunk/api-gateway-enable-cors/aws"
   version = "0.3.3"
   api_id          = aws_api_gateway_rest_api.dga-apigw.id
-  for_each = toset( [aws_api_gateway_rest_api.dga-apigw.root_resource_id, aws_api_gateway_resource.boards.id] )
+  for_each = toset( [aws_api_gateway_rest_api.dga-apigw.root_resource_id, aws_api_gateway_resource.proxy.id] )
   api_resource_id = each.key
 }
 
@@ -38,7 +38,7 @@ resource "aws_api_gateway_method" "any" {
   rest_api_id   = aws_api_gateway_rest_api.dga-apigw.id
 }
 resource "aws_api_gateway_integration" "proxy" {
-  http_method = aws_api_gateway_method.proxy.http_method
+  http_method = aws_api_gateway_method.any.http_method
   resource_id = aws_api_gateway_resource.proxy.id
   rest_api_id = aws_api_gateway_rest_api.dga-apigw.id
   type                    = "HTTP"
