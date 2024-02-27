6b9d4337a0598beef98671c5087916696549ebf5
HoneyBIN <binhao@naver.com>
2024-02-27 16:31:11 +0900

    cognito_apigw

--- env/woobin/module/cognito/main.tf
+++ env/woobin/module/cognito/main.tf
@@ -47,6 +47,8 @@ module "dga-userpool" {
   email_configuration = {
     email_sending_account  = "COGNITO_DEFAULT"
   }
+  # 앱을 통해 가입 가능
+  admin_create_user_config_allow_admin_create_user_only = flase
   # 사용자 계정 복구 방법
   recovery_mechanisms = [
      {
