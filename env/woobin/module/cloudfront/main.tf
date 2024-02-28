a5bd52e3d176df6466db687c6e5a894ffd3becd7
HoneyBIN <binhao@naver.com>
2024-02-28 20:00:25 +0900

    adn

--- env/woobin/module/cloudfront/main.tf
+++ env/woobin/module/cloudfront/main.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
-    origin_id   = var.domain
+    origin_id   = "dga-s3"
     domain_name = var.s3-id
     custom_origin_config {
       http_port              = 80
@@ -10,14 +10,25 @@ resource "aws_cloudfront_distribution" "cdn" {
     }
   }
 
+  origin {
+    origin_id   = "dga-apigw"
+    domain_name = var.apigw-id
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+  }
+
   aliases = ["www.${var.domain}"]
 
   enabled             = true
 
   default_cache_behavior {
+    target_origin_id = "dga-s3"
     allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "${var.domain}.s3.amazonaws.com"
 
     forwarded_values {
       query_string = false
@@ -33,10 +44,10 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   ordered_cache_behavior {
+    target_origin_id = "dga-apigw"
     path_pattern     = "/api/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "dga-apigw"
 
     forwarded_values {
       query_string = false
