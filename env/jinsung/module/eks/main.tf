febba8a0329e9d9ecb51b880baa6eafa9953489c
김진성 <winnertree@naver.com>
2024-03-03 00:00:44 +0900

    helm argo

--- env/jinsung/module/eks/main.tf
+++ env/jinsung/module/eks/main.tf
@@ -364,11 +364,11 @@ resource "kubernetes_ingress_v1" "alb6" {
   }
 }
 
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
+# resource "kubernetes_namespace" "argocd" {
+#   metadata {
+#     name = "argocd"
+#   }
+# }
 
 resource "helm_release" "argocd" {
   name       = "admin"
