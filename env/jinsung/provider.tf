69917053e10e55193c618b21dc0aabf689dc5205
김진성 <winnertree@naver.com>
2024-03-02 21:51:36 +0900

    change module

--- env/jinsung/provider.tf
+++ env/jinsung/provider.tf
@@ -4,14 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    # helm = {
-    #   source  = "hashicorp/helm"
-    #   version = "~> 2.0"
-    # }
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = ">= 2.0.0"
-    # }
+    k8s = {
+        source = "banzaicloud/k8s"
+        version = ">= 0.8.0"
+    }
   }
 }
 
@@ -19,17 +15,17 @@ provider "aws" {
   region = var.region
 }
 
-# #
-provider "helm" {
-  kubernetes {
-    host                   = module.dga-eks.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.dga-eks.eks_cluster_certificate_authority_data)
-    #token                  = module.dga-eks.token
-  }
-}
+# # #
+# provider "helm" {
+#   kubernetes {
+#     host                   = module.dga-eks.eks_cluster_endpoint
+#     cluster_ca_certificate = base64decode(module.dga-eks.eks_cluster_certificate_authority_data)
+#     #token                  = module.dga-eks.token
+#   }
+# }
 
-provider "kubernetes" {
-  host                   = module.dga-eks.eks_cluster_endpoint
-  cluster_ca_certificate = base64decode(module.dga-eks.eks_cluster_certificate_authority_data)
-  #token                  = module.dga-eks.token
-}
+# provider "kubernetes" {
+#   host                   = module.dga-eks.eks_cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.dga-eks.eks_cluster_certificate_authority_data)
+#   #token                  = module.dga-eks.token
+# }
