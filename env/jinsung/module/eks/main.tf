427344d568a21f061b9fab0bb82fcb9da265f926
김진성 <winnertree@naver.com>
2024-03-02 21:15:28 +0900

    rest

--- env/jinsung/module/eks/main.tf
+++ env/jinsung/module/eks/main.tf
@@ -378,7 +378,7 @@ module "argocd" {
   source = "squareops/argocd/kubernetes"
   argocd_config = {
     hostname                     = "argocd.prod.in"
-    values_yaml                  = file("module/eks/helm/values.yaml")
+    values_yaml                  = file("./module/eks/helm/values.yaml")
     redis_ha_enabled             = true
     autoscaling_enabled          = true
     slack_notification_token     = ""
