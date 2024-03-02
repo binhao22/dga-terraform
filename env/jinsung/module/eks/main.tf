70eb28eb25aba34a282c3ff5d89f951fae680e37
김진성 <winnertree@naver.com>
2024-03-03 01:07:20 +0900

    ingress

--- env/jinsung/module/eks/main.tf
+++ env/jinsung/module/eks/main.tf
@@ -380,7 +380,7 @@ resource "kubernetes_ingress_v1" "alb6" {
 locals {
   # Example annotations when using Nginx ingress controller as shown here https://argoproj.github.io/argo-cd/operator-manual/ingress/#option-1-ssl-passthrough
   argocd_ingress_annotations = {
-    "kubernetes.io/ingress.class" = nginx
+    "kubernetes.io/ingress.class" = "nginx"
     "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
     "nginx.ingress.kubernetes.io/ssl-passthrough" = "true"
   }
