1f6bc8e1d2665812d0c70c0b701767e24ae59d22
winnertree <winnertree@naver.com>
2024-03-04 16:00:46 +0900

    create argocd

--- env/jinsung/module/eks/main.tf
+++ env/jinsung/module/eks/main.tf
@@ -374,7 +374,7 @@ resource "kubernetes_namespace" "argocd" {
 # argocd namespace 생성
 
 resource "kubernetes_manifest" "argo_ingress" {
-  manifest = yamldecode(file(".jinsung/helm/argo_ingress.yml"))
+  manifest = yamldecode(file("./module/eks/helm/argo_ingress.yml"))
 }
 
 resource "helm_release" "argocd" {
